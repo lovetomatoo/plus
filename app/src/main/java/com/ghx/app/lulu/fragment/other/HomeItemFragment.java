6feff922e6149dccdb4bd4413913de57955f1ae7
guohongxin <736685433@qq.com>
2016-09-27 19:07:36 +0800

    首页ViewPager轮播图
    首页ViewPager轮播图

--- app/src/main/java/com/ghx/app/lulu/fragment/other/HomeItemFragment.java
+++ app/src/main/java/com/ghx/app/lulu/fragment/other/HomeItemFragment.java
@@ -1,6 +1,9 @@
 package com.ghx.app.lulu.fragment.other;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.ghx.app.R;
@@ -8,14 +11,19 @@ import com.ghx.app.base.BaseFragment;
 import com.ghx.app.lulu.presenter.HomeItemFragmentPresenter;
 import com.ghx.app.lulu.view.IHomeItemFragmentView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by guo_hx on 2016/9/26.17:10
  */
 
 public class HomeItemFragment extends BaseFragment<HomeItemFragmentPresenter> implements IHomeItemFragmentView {
 
+    List<View> mViewList = new ArrayList<>();
+
     private String mFlag;
-    private TextView mTvHomeViewPagerItem;
+    private ViewPager mVpAuto;
 
     @Override
     protected int getLayoutId() {
@@ -30,10 +38,59 @@ public class HomeItemFragment extends BaseFragment<HomeItemFragmentPresenter> im
     @Override
     protected void initAllWidget(View rootView) {
 
-        mTvHomeViewPagerItem = (TextView) rootView.findViewById(R.id.tv_home_viewpager_item);
-        mTvHomeViewPagerItem.setText(mFlag);
+        mVpAuto = (ViewPager) rootView.findViewById(R.id.vp_auto);
 
-//        mPresenter.getServerData(mFlag);
+        for (int i = 0; i < 5; i++) {
+            TextView textView = new TextView(getActivity());
+            View view = View.inflate(getActivity(), R.layout.item_auto_viewpager, null);
+            switch (mFlag) {
+                case "id = " + 0:
+                    textView.setText(i + "00000哈哈，我是都比，你能咋地");
+                    break;
+                case "id = " + 1:
+                    textView.setText(i + "11111哈哈，我是都比，你能咋地");
+                    break;
+                case "id = " + 2:
+                    textView.setText(i + "22222哈哈，我是都比，你能咋地");
+                    break;
+                case "id = " + 3:
+                    textView.setText(i + "33333哈哈，我是都比，你能咋地");
+                    break;
+                case "id = " + 4:
+                    textView.setText(i + "44444哈哈，我是都比，你能咋地");
+                    break;
+            }
+            mViewList.add(view);
+        }
+
+        mVpAuto.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return 5;
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+//                return super.instantiateItem(container, position);
+                ((ViewPager) container).addView(mViewList.get(position), 0);
+
+                return mViewList.get(position);
+            }
+
+            @Override
+            public void destroyItem(View view, int position, Object object) {
+                ((ViewPager) view).removeView(mViewList.get(position));
+            }
+
+
+        });
+
+
         mPresenter.getServerData(mFlag);
 
     }
