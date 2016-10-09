22d8ad7f263a392bd4bd1040e09976f26474933c
guohongxin <736685433@qq.com>
2016-10-09 19:26:36 +0800

    删除PullLoadMore
    删除PullLoadMore

--- app/src/main/java/com/ghx/app/lulu/fragment/other/HomeItemFragment.java
+++ app/src/main/java/com/ghx/app/lulu/fragment/other/HomeItemFragment.java
@@ -6,11 +6,8 @@ import com.ghx.app.R;
 import com.ghx.app.base.BaseFragment;
 import com.ghx.app.lulu.model.LunbotuBean;
 import com.ghx.app.lulu.presenter.HomeItemFragmentPresenter;
-import com.ghx.app.lulu.utils.ToastUtil;
 import com.ghx.app.lulu.view.IHomeItemFragmentView;
 import com.ghx.app.lulu.weiget.autoscroll_viewpager.AutoScrollViewPager;
-import com.ghx.app.lulu.weiget.pullloadmore_listview.AnimatePullLoadMoreListView;
-import com.ghx.app.lulu.weiget.pullloadmore_listview.PullLoadMoreListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,13 +16,12 @@ import java.util.List;
  * Created by guo_hx on 2016/9/26.17:10
  */
 
-public class HomeItemFragment extends BaseFragment<HomeItemFragmentPresenter> implements IHomeItemFragmentView, PullLoadMoreListener {
+public class HomeItemFragment extends BaseFragment<HomeItemFragmentPresenter> implements IHomeItemFragmentView {
 
     List<String> mPicUrlList = new ArrayList<>();
 
     private String mFlag;
     private AutoScrollViewPager mVpAuto;
-    private AnimatePullLoadMoreListView mMoreListView;
 
     @Override
     protected int getLayoutId() {
@@ -41,10 +37,6 @@ public class HomeItemFragment extends BaseFragment<HomeItemFragmentPresenter> im
     protected void initAllWidget(View rootView) {
 
         mVpAuto = (AutoScrollViewPager) rootView.findViewById(R.id.vp_auto);
-//        mMoreListView = (AnimatePullLoadMoreListView) rootView.findViewById(R.id.more_list_view);
-//        mMoreListView.setPullLoadMoreListener(this);
-
-
     }
 
     @Override
@@ -64,15 +56,4 @@ public class HomeItemFragment extends BaseFragment<HomeItemFragmentPresenter> im
         mVpAuto.setBorderAnimation(false);
     }
 
-    @Override
-    public void onRefresh() {
-
-        ToastUtil.showToast("onRefresh");
-    }
-
-    @Override
-    public void onLoadMore() {
-
-        ToastUtil.showToast("onLoadMore");
-    }
 }
